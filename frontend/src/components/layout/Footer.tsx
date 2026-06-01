98e0559c6b6c0521bfd1c57a42f967f58f6bd42a
Rye Stahle-Smith <rstahle-smith@swampfox.com>
2026-06-01 16:37:41 -0400

    Updated login page background + footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -5,22 +5,25 @@
 
 export function Footer() {
   return (
-    <footer className="border-t border-gray-200 bg-white px-3 sm:px-6 py-4 sticky bottom-0 z-10 shadow-sm">
-      <div className="flex flex-col md:flex-row justify-between items-center max-w-7xl mx-auto space-y-4 md:space-y-0">
-        <div className="flex space-x-12">
-          <p className="text-sm text-zinc-500">
+    <footer className="border-t border-gray-200 bg-white px-3 sm:px-6 py-3 sticky z-10 shadow-sm">
+      <div className="flex justify-between max-w-8xl">
+        <div className="flex">
+          <p className="text-sm text-black sm:hidden">
+            © 2026, Rye Stahle-Smith.
+          </p>
+          <p className="text-sm text-black hidden sm:inline">
             Copyright © 2026, Rye Stahle-Smith; All rights reserved.
           </p>
         </div>
-        <div className="flex space-x-12">
+        <div className="flex space-x-8">
           <a
-            className="text-zinc-500 text-sm hover:text-gray-900 transition-colors"
+            className=" text-black text-sm hover:text-gray-600 transition-colors"
             href={`${import.meta.env.VITE_SSE_BASE_URL}/docs`}
           >
             API Documentation
           </a>
           <a
-            className="text-zinc-500 text-sm hover:text-gray-900 transition-colors"
+            className=" text-black text-sm hover:text-gray-600 transition-colors"
             href="https://github.com/Bread2002/Gmail_Cleaner"
           >
             GitHub
